7086523e71e5ee78777224eafb4a6dfce4cf9747
Skyteeee <spusi.vapus@gmail.com>
2022-07-31 22:10:32 -0500

    Initialized project, added tilemap, character movement + animations

---
+++ Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControlls : MonoBehaviour
+{
+
+    Vector2 movementValue = Vector2.zero;
+    Rigidbody2D rb;
+    Animator animator;
+    public float moveSpeed = 1f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementValue != Vector2.zero)
+        {
+            rb.MovePosition(rb.position + movementValue * Time.fixedDeltaTime * moveSpeed);
+
+        }
+
+        SwitchAnimationsIfNeeded();
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        this.movementValue = movementValue.Get<Vector2>();
+    }
+
+
+    void SwitchAnimationsIfNeeded()
+    {
+        if (movementValue.x > 0)
+        {
+            animator.SetBool("facingRight", true);
+        }
+        if (movementValue.x < 0)
+        {
+            animator.SetBool("facingRight", false);
+        }
+
+        animator.SetBool("isMoving", movementValue != Vector2.zero);
+
+    }
+
+
+}
